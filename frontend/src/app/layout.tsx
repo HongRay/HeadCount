c8a46c6a3ddcc09e1cda6901b816ac5af213f920
Kenneth Seet <kenurfseet@gmail.com>
2024-09-29 00:27:48 -0700

    Modify layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Card, CardBody } from "@nextui-org/react";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <Card className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <CardBody className="bg-white p-8 rounded-md shadow-md text-center w-full sm:w-96">
+              {children}
+            </CardBody>
+          </Card>
+        </div>
       </body>
     </html>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { Card, CardBody, Button, Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -61,77 +61,69 @@ export default function Login() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Card className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <CardBody className="bg-white p-8 rounded-md shadow-md text-center w-full sm:w-96">
-          {/* Logo */}
-          <Image
-            src="/images/logo.png"
-            alt="HeadCount"
-            width={48}
-            height={48}
-            className="mx-auto h-12 w-auto"
+    <>
+      {/* Logo */}
+      <Image
+        src="/images/logo.png"
+        alt="HeadCount"
+        width={48}
+        height={48}
+        className="mx-auto h-12 w-auto"
+      />
+      <h1 className="text-2xl font-bold mt-4">Sign in to HeadCount</h1>
+      <p className="text-gray-500 text-sm mt-2">
+        Welcome back! Please sign in to view the dashboard.
+      </p>
+      {/* Render Error Message */}
+      {errorMessage && (
+        <div className="text-red-500 text-sm mt-4 mb-4 p-2 bg-red-100 rounded-md">
+          {errorMessage}
+        </div>
+      )}
+      <div className="mt-6 space-y-4">
+        {/* Email Input */}
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+          <Input
+            isRequired
+            type="email"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full"
+            required
           />
+        </div>
 
-          <h1 className="text-2xl font-bold mt-4">Sign in to HeadCount</h1>
-          <p className="text-gray-500 text-sm mt-2">
-            Welcome back! Please sign in to view the dashboard.
-          </p>
+        {/* Password Input */}
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+          <Input
+            isRequired
+            type="password"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyPress={handleKeyPress} // Attach the keypress event
+            className="w-full"
+            required
+          />
+        </div>
 
-          {/* Render Error Message */}
-          {errorMessage && (
-            <div className="text-red-500 text-sm mt-4 mb-4 p-2 bg-red-100 rounded-md">
-              {errorMessage}
-            </div>
-          )}
-
-          <div className="mt-6 space-y-4">
-            {/* Email Input */}
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                isRequired
-                type="email"
-                label="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full"
-                required
-              />
-            </div>
-
-            {/* Password Input */}
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                isRequired
-                type="password"
-                label="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onKeyPress={handleKeyPress} // Attach the keypress event
-                className="w-full"
-                required
-              />
-            </div>
-
-            {/* Login Button */}
-            <Button
-              onClick={handleLoginClick}
-              color="warning"
-              className="w-full py-2"
-            >
-              Continue
-            </Button>
-          </div>
-
-          {/* "Forgot Password?" and "Sign Up" Links */}
-          <div className="mt-4 text-sm text-gray-600">
-            Don&apos;t have an account?&nbsp;
-            <a href="/register" className="hover:underline">
-              Contact Sales
-            </a>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+        {/* Login Button */}
+        <Button
+          onClick={handleLoginClick}
+          color="warning"
+          className="w-full py-2"
+        >
+          Continue
+        </Button>
+      </div>
+      {/* "Forgot Password?" and "Sign Up" Links */}
+      <div className="mt-4 text-sm text-gray-600">
+        Don&apos;t have an account?&nbsp;
+        <a href="/register" className="hover:underline">
+          Contact Sales
+        </a>
+      </div>
+    </>
   );
 }
